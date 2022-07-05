b9ebcc2b72815e45bc46e4e05d3c7fdfc04ecbd0
Rodrigo <digo.s.oliv@gmail.com>
2022-07-04 21:33:45 -0300

    Treinando if e else

--- aluramidi/main-for.js
+++ aluramidi/main-for.js
@@ -3,16 +3,13 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 function tocaSom(seletorAudio){
     const elemento = document.querySelector(seletorAudio)
 
-    if (elemento === null){
-        console.log('Elemento não encontrado')
-    }
     
-    if (elemento != null ){
-
-        if (elemento.localName === 'audio'){
+    if (elemento != null && elemento.localName === 'audio' ){
             elemento.play();
         }
-    }
+        else{
+            console.log('Elemento não encontrado ou seletor inválido')
+        }
 }
 
 
