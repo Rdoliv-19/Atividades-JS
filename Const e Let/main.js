2d8365bb8e5b0a519ab3d2cc30ce7f6716ccacf7
Rodrigo <digo.s.oliv@gmail.com>
2022-08-15 22:32:10 -0300

    Atividade const e let

---
+++ Const e Let/main.js
@@ -0,0 +1,10 @@
+const nome = "Rodrigo Silva";
+const sobrenome = "Oliveira";
+const idade = 31;
+const peso = 98;
+const altura = 1.75;
+let imc = (peso / (altura * altura))
+let anoNascimento = 31 - 1991
+
+console.log(nome + " " + sobrenome + " tem " + idade + " anos, pesa " + peso + "Kg, tem " 
++ altura + " e seu IMC é de " + imc + nome + "nasceu em" + anoNascimento)
