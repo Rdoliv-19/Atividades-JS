0adabf6b81e4710424f612130efeb0cfe3d98e07
Rodrigo <digo.s.oliv@gmail.com>
2022-09-21 15:06:00 -0300

    Exercícios de JavaScript

---
+++ Operação Ternaria/ternaria.js
@@ -0,0 +1,6 @@
+const pontuacaoUsuario = 500
+
+//                          CONDIÇÃO           VALOR VERDADEIRO    VALOR FALSO
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuário normal'
+
+console.log(nivelUsuario)
