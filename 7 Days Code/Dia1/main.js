a626c7ba0f5e4994d5ddea125364a3262dee9cc0
Rodrigo <digo.s.oliv@gmail.com>
2022-09-23 09:51:01 -0300

    Operações com comparadores de comparação

---
+++ 7 Days Code/Dia1/main.js
@@ -0,0 +1,24 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta  = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (numeroUm == stringUm){
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor mas tipos diferentes')
+}else{
+    console.log('As variáveis não tem o mesmo valor')
+}
+
+if (numeroTrinta === stringTrinta){
+    console.log('Tem o mesmo valor e mesmo tipo')
+}else{
+    console.log('As variáveis 30 não são do mesmo tipo')
+}
+
+if (numeroDez !== stringDez){
+    console.log('As variáveis de numero 10 são do mesmo valor mas tipos diferentes')
+}else{
+    console.log('As variáveis 10 são do mesmo valor e mesmo tipo')
+}
