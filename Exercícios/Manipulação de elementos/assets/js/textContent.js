63c9defeb934ea06641c86f525e9fe742baa3a8c
Rodrigo <digo.s.oliv@gmail.com>
2022-11-03 18:09:36 -0300

    Treinamento DOM

---
+++ Exercícios/Manipulação de elementos/assets/js/textContent.js
@@ -0,0 +1,15 @@
+let titulo = document.querySelector("h1");
+let container = document.querySelector(".container")
+
+console.log(container.children)
+
+
+let div = document.createElement("div")
+
+div.innerText = "Nova Div"
+
+titulo.appendChild(div)
+
+
+
+
--- forEach/forEach.js
+++ forEach/forEach.js
@@ -1,8 +1,14 @@
-let pacientes = document.querySelectorAll(".paciente")
+// let pacientes = document.querySelectorAll(".paciente")
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        console.log("Clicado duas vezes")
-        this.remove();
-    }) 
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener("dblclick", function(){
+//         console.log("Clicado duas vezes")
+//         this.remove();
+//     }) 
+// })
+
+let marcas = ["Lenovo", "HP", "Dell"]
+
+marcas.forEach(function(marcas, index, array){
+    console.log(marcas, index, array)
 })
